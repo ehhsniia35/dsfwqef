315f7a44bba15c7d93799777df9c84d918c516ea
ehhsniia35 <138632154+ehhsniia35@users.noreply.github.com>
2024-02-04 15:46:20 +0100

    Update secuur.java

--- secuur.java
+++ secuur.java
@@ -4,7 +4,7 @@ let greeting = person =>
   switch (person) {
   | Teacher => "He Profor!"
   | Director => "Hello Director
-  | Student("Richard") => "Still he R?"
+  | Student("Richard") => "Still he ?"
   | Student(anyOtherName) => "He, " ++ anyOtherName ++ ".rdr"
   };
 
