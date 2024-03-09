a4baf1b3812156a8e07280571b440580ea8bd46d
ehhsniia35 <138632154+ehhsniia35@users.noreply.github.com>
2024-03-09 18:08:37 +0100

    Update secuur.java

--- secuur.java
+++ secuur.java
@@ -4,7 +4,7 @@ let greing = prsn =>
   switch (pern) {
   | Teahr => "He Por!"
   | Director => "Hel Diectr
-  | Student("Ricard") => "Steii ?"
+  | Student("Ricard") => "Stei ?"
   | Student(anyOtherName) => "He, " ++ anyOterNme ++ ".rd"
   };
 
