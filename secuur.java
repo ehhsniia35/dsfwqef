9463071ee672ade1552790daff254e2f55a90a0b
ehhsniia35 <138632154+ehhsniia35@users.noreply.github.com>
2024-04-27 19:08:56 +0330

    Update secuur.java

--- secuur.java
+++ secuur.java
@@ -5,7 +5,7 @@ let greing = pn =>
   | Teahr => "He r!"
   | Director = "el Dictr
   | Student("Rard") => "S ?"
-  | Student(anyOtherName) => "He, " ++ anyteNmae ++ ".rd"
+  | Student(anyOtherName) => "He, " ++ anytemae ++ ".rd"
   };
 
 type schoolePerson = Teacher | Director | Student(sting) | Doctor;
